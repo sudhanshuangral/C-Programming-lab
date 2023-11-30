cbb260fdff4d736ea6bffc5548bf5b455d2e3558
Sudhanshu_angral <147260436+sudhanshuangral@users.noreply.github.com>
2023-11-30 22:49:22 +0530

    star.c
    to print '*' matrix.

---
+++ star.c
@@ -0,0 +1,22 @@
+//to print '*' matrix.
+#include<stdio.h>
+void main()
+{
+int i,j,rows=4;
+printf("The matrix is:\n");
+for(i=0;i<=4;++i)
+{
+for(j=0;j<=rows;++j)
+{
+printf("*");
+}
+printf("\n");
+}
+}
+
+
+OUTPUT:-
+****
+****
+****
+****
