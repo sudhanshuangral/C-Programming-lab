47af38a336080ed947e6cc11d06c1058134a81f4
Sudhanshu_angral <147260436+sudhanshuangral@users.noreply.github.com>
2023-11-30 22:45:49 +0530

    four_numbers.c
    to find the greatest of four numbers.

---
+++ four_numbers.c
@@ -0,0 +1,10 @@
+//to find the greatest of four numbers.
+#include<stdio.h>
+void main()
+{
+int a,b,c,d,big;
+printf("Enter four numbers:");
+scanf("%d%d%d%d",&a,&b,&c,&d);
+big=a>b?(a>c?(a>d?a:d):(c>d?c:d)):(b>c?(b>d?b:d):(c>d?c:d));
+printf("The greatest number is:%d",big);
+}
